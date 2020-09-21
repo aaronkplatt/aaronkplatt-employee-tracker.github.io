a5ef85d67744072c3e13f5b04a22e5de42b82c0b
Aaron Platt <aaron.k.platt@gmail.com>
2020-09-21 14:55:00 -0700

    testing the add employee

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { doesNotMatch } = require('assert');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 const mysql = require('mysql');
@@ -37,9 +38,9 @@ function initialQuestion() {
         {
             type: "list",
             name: "initialQuestion",
-            message: "What is this team-members role?",
+            message: "What would you like to do?",
             choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", "Remove Employee", "Update Employee Role", "Update Employee Manager"]
-        },
+        }
     ])
     .then(function(answers) {
         const initialQuestion = answers.initialQuestion;
@@ -47,29 +48,86 @@ function initialQuestion() {
             viewAllEmployees();
         }
         else if (initialQuestion === "View All Employees By Department") {
-            //View All Employees By Department
+            viewAllEmployeesDepartment();
         }
         else if (initialQuestion === "View All Employees By Manager") {
-            //View All Employees By Manager
+            viewAllEmployeesManager();
         }  
         else if (initialQuestion === "Add Employee") {
-            //Add Employee
+            addEmployee();
         }  
         else if (initialQuestion === "Remove Employee") {
-            //Remove Employee
+            removeEmployee();
         }  
         else if (initialQuestion === "Update Employee Role") {
-            //Update Employee Role
+            updateEmployeeRole();
         }  
         else if (initialQuestion === "Update Employee Manager") {
-            //Update Employee Manager
-        }  
+            updateEmployeeManager();
+        } 
     });
 }    
+
+//VIEW ALL EMPLOYEES
 function viewAllEmployees() {
     //console.table makes the table in the command line and allEmployees is the array at the top of the page 
     console.table(['id', 'first_name', 'last_name', 'title', 'department', 'salary', 'manager'], allEmployees);
-
-
+    //go back to inital
+    initialQuestion();
 } 
+
+//VIEW ALL EMPLOYEES BY DEPARTMENT
+function viewAllEmployeesDepartment() {
+    console.log("working");
+    //go back to inital
+    initialQuestion();
+}
+
+//VIEW ALL EMPLOYEES BY MANAGER
+function viewAllEmployeesManager() {
+    console.log("working");
+    //go back to inital
+    initialQuestion();
+}
+
+//ADD EMPLOYEE
+function addEmployee() {
+    console.log("working");
+    inquirer
+    .prompt([
+        //WHAT WOULD YOU LIKE TO DO? (initialQuestion)
+        {
+            type: "input",
+            name: "first_name",
+            message: "What is this Employee's First Name?",
+        }
+    ])
+    .then(function(answers) {
+        const first_name = answers.first_name;
+        console.log(first_name);
+    });
+    //go back to inital
+    // initialQuestion();
+}
+
+//REMOVE EMPLOYEE
+function removeEmployee() {
+    console.log("working");
+    //go back to inital
+    initialQuestion();
+}
+
+//UPDATE EMPLOYEE ROLE
+function updateEmployeeRole() {
+    console.log("working");
+    //go back to inital
+    initialQuestion();
+}
+
+//UPDATE EMPLOYEE MANAGER
+function updateEmployeeManager() {
+    console.log("working");
+    //go back to inital
+    initialQuestion();
+}
 
