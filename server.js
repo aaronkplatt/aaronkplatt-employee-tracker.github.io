017b7dbcbdd1623dad52d4135552cbed68f3f6c3
Aaron Platt <aaron.k.platt@gmail.com>
2020-09-24 18:14:06 -0700

    I finished the first three functions and got them working

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mysql = require('mysql');
 
 //Express connection
 const app = express();
-const PORT = 8080;
+const PORT = 3306;
 
 app.use(express.urlencoded({
     extended: true
@@ -17,7 +17,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     // Your port; if not 3306
     port: 3306,
-  
+    
     // Your username
     user: "root",
     // Your password
@@ -30,7 +30,20 @@ connection.connect(function(err) {
     console.log("connected as id " + connection.threadId + "\n");
 });
 
-//THIS IS THE 1ST THING THAT WHILL HAPPEN WHEN THE APPLICATION IS RUN (NOT FULLY WORKING)
+//EMPTY OBJECTS with ID's
+let employeeListArray = {}; // havent used yet
+let managerListArray = {}; //havent used yet
+let roleListArray = {}; // havent used yet
+let departmentListArray = {}; // havent used yet
+
+//EMPTY ARRAYS
+let importEmployeeArray = []; //havent used yet
+let importManagersArray = ["Null"]; //NULL IF NO MANAGER
+let importRoleArray = [] //havent used yet
+let importDepartmentArray = []; //havent used yet
+
+
+//THIS IS THE 1ST THING THAT WILL HAPPEN WHEN THE APPLICATION IS STARTED (NOT FULLY WORKING)
 initialQuestion();
 function initialQuestion() {
     inquirer
@@ -40,7 +53,7 @@ function initialQuestion() {
             type: "list",
             name: "initialQuestion",
             message: "What would you like to do?",
-            choices: ["View All Employees", "View All Employees By Department", /*"View All Employees By Manager",*/ "Add Employee", /*"Remove Employee",*/ /*"Update Employee Role",*/ /*"Update Employee Manager"*/]
+            choices: ["View All Employees", "View All Employees By Department", "View All Employees By Manager", "Add Employee", /*"Remove Employee",*/ /*"Update Employee Role",*/ /*"Update Employee Manager"*/]
         }
     ])
     .then(function(answers) {
@@ -83,8 +96,6 @@ function viewAllEmployees() {
                 console.log("\n");
         //console.table makes the table in the command line and allEmployees is the array at the top of the page 
         console.table(res);
-        
-        connection.end();
         //go back to inital
         initialQuestion();
     });
@@ -92,101 +103,137 @@ function viewAllEmployees() {
 
 //VIEW ALL EMPLOYEES BY DEPARTMENT (WORKING)
 function viewAllEmployeesDepartment() {
-    // console.log("working");
-    inquirer
-    .prompt([
-        {
-            type: "list",
-            name: "viewAllEmployeesDepartment",
-            message: "From which department would you like to see the Employees?",
-            choices: ["Engineering", "Finance", "Legal", "Sales"]
-        }
-    ])
-    .then(function(answers) {
-        const viewAllEmployeesDepartment = answers.viewAllEmployeesDepartment;
-        connection.query(
-            `SELECT concat(e.first_name, ' ', e.last_name) AS name, d.department 
-            FROM employee_tracker_db.employee AS e
-            JOIN employee_tracker_db.role AS r ON e.role_id = r.id
-            JOIN employee_tracker_db.department AS d ON r.department_id = d.id
-            WHERE department = "${viewAllEmployeesDepartment}"`,
-            function(err, res) {
-                if (err) throw err;
-                // Log all results of the SELECT statement
-                console.log("\n");
-                //console.table makes the table in the command line and allEmployees is the array at the top of the page 
-                console.table(res);
-                
-                connection.end();
-                //go back to inital
-                initialQuestion();
-            });
+    let importDepartmentArray = []; //MAKING IT 0 so they dont duplicate
+    connection.query(`SELECT department FROM employee_tracker_db.department;`, (err,rows) => {
+        if(err) throw err;
+        rows.forEach((row) => {
+            importDepartmentArray.push(`${row.department}`)
         });
-    //go back to inital
-    // initialQuestion();
-}
-
-//VIEW ALL EMPLOYEES BY MANAGER (HAVENT STARTED)
-function viewAllEmployeesManager() {
-    console.log("working");
-    //go back to inital
-    initialQuestion();
-}
-
-//ADD EMPLOYEE (NOT WORKING)
-function addEmployee() {
-    console.log("working");
-    inquirer
-    .prompt([
-        //WHAT WOULD YOU LIKE TO DO? (initialQuestion)
-        {
-            type: "input",
-            name: "first_name",
-            message: "What is this Employee's First Name?"
-        },
-        {
-            type: "input",
-            name: "last_name",
-            message: "What is this Employee's Last Name?"
-        },
-        {
-            type: "list",
-            name: "role_title",
-            message: "What is this Employee's role?",
-            choices: ["Sales Lead", "Salesperson", "Lead Engineer", "Software Engineer", "Account Manager", "Accountant", "Legal Team Lead", "Lawyer"]
-        }
-    ])
-    .then(function(answers) {
-        const first_name = answers.first_name;
-        const last_name = answers.last_name;
-        const role_title = answers.role_title;
-        // console.log(answers);
-
-        //IMPORTANT This is going to thow you into another inquirer function to ask for the Managers in MYSQL
-        importManagers();
-    });
-}
-// THIS IS PART OF ADD EMPLOYEE, NEED to get managers from mysql (NOT WORKING)
-function importManagers() {
-
-    
-        // console.log(res);
         inquirer
         .prompt([
             {
                 type: "list",
-                name: "employee_manager",
-                message: "Which Manager do you want to set for the Employee? (Null if No manager)",
-                choices: [connection.query(`SELECT concat(first_name, ' ', last_name) AS NAME FROM employee_tracker_db.employee`)] //figure out
+                name: "viewAllEmployeesDepartment",
+                message: "From which department would you like to see the Employees?",
+                choices: importDepartmentArray
             }
         ])
-.then(function(answers) {
-    const employee_manager = answers.employee_manager;
-    // console.log(answers);
-    //go back to inital
-    initialQuestion();
-});
-} //THIS FUNCTION IS PART OF ADDEMPLOYEE()
+        .then(function(answers) {
+            const viewAllEmployeesDepartment = answers.viewAllEmployeesDepartment;
+            connection.query(
+                `SELECT concat(e.first_name, ' ', e.last_name) AS name, d.department 
+                FROM employee_tracker_db.employee AS e
+                JOIN employee_tracker_db.role AS r ON e.role_id = r.id
+                JOIN employee_tracker_db.department AS d ON r.department_id = d.id
+                WHERE department = "${viewAllEmployeesDepartment}"`,
+                function(err, res) {
+                    if (err) throw err;
+                    console.log("\n");
+                    // Log all results of the SELECT statement
+                    console.table(res);
+                    //go back to inital
+                    initialQuestion();
+                });
+            });
+        });
+}
+
+//VIEW ALL EMPLOYEES BY MANAGER (WORKING)
+function viewAllEmployeesManager() {
+    // IMPORT MANAGER
+    let importManagersArray = ["Null"];
+    connection.query(`SELECT concat(first_name, ' ', last_name) AS NAME FROM employee_tracker_db.employee`, (err,rows) => {
+        if(err) throw err;
+        rows.forEach((row) => {
+            importManagersArray.push(`${row.NAME}`)
+        });
+        inquirer
+        .prompt([
+            {
+                type: "list",
+                name: "viewAllEmployeesManager",
+                message: "Which Manager would you like to view?",
+                choices: importManagersArray
+            }
+        ])
+        .then(function(answers) {
+            const viewAllEmployeesManager = answers.viewAllEmployeesManager
+            console.log(viewAllEmployeesManager);
+            connection.query(`SELECT e.first_name, e.last_name, concat(m.first_name, ' ', m.last_name) AS manager 
+            FROM employee_tracker_db.employee AS e 
+            LEFT JOIN employee_tracker_db.employee AS m ON e.manager_id = m.id 
+            JOIN employee_tracker_db.role AS r ON e.role_id = r.id
+            JOIN employee_tracker_db.department AS d ON r.department_id = d.id
+            WHERE concat(m.first_name, ' ', m.last_name) = "${viewAllEmployeesManager}";`,
+            function(err, res) {
+                if (err) throw err;
+                console.log("\n");
+                // Log all results of the SELECT statement
+                console.table(res);
+                //go back to inital
+                initialQuestion();
+            });
+            //go back to inital
+        });
+    });
+}
+
+//ADD EMPLOYEE (NOT WORKING)
+function addEmployee() {
+    let importManagersArray = ["Null"];
+    let importRoleArray = [];
+    connection.query(`SELECT concat(first_name, ' ', last_name) AS NAME, r.title AS ROLE
+    FROM employee_tracker_db.employee AS e
+    JOIN employee_tracker_db.role AS r ON e.role_id = r.id`, (err,rows) => {
+        if(err) throw err;
+        rows.forEach((row) => {
+            importManagersArray.push(`${row.NAME}`)
+            importRoleArray.push(`${row.ROLE}`)
+        });
+        inquirer
+        .prompt([
+            //WHAT WOULD YOU LIKE TO DO? (initialQuestion) //import role!, 
+            {
+                type: "input",
+                name: "first_name",
+                message: "What is this Employee's First Name?"
+            },
+            {
+                type: "input",
+                name: "last_name",
+                message: "What is this Employee's Last Name?"
+            },
+            {
+                type: "list",
+                name: "role_title",
+                message: "What is this Employee's role?",
+                choices: importRoleArray
+            },
+            {
+                type: "list",
+                name: "employee_manager",
+                message: "Which Manager do you want to set for the Employee? (Null if No manager)",
+                choices: importManagersArray 
+            }
+        ])
+        .then(function(answers) {
+            first_name = answers.first_name,
+            last_name = answers.last_name,
+            role_title = answers.role_title,
+            employee_manager = answers.employee_manager,
+            connection.query(`INSERT INTO employee_tracker_db.employee (first_name,last_name,role_id,manager_id)
+            VALUES ("${first_name}","${last_name}",3,4)`,//DAD WILL HELP 
+            
+                function(err) {
+                    if(err) throw err;
+                    
+                    console.log("EMPLOYEE ADDED!")
+                    initialQuestion();
+                }
+            );
+        });
+    });
+}
 
 //REMOVE EMPLOYEE (havent started)
 function removeEmployee() {
